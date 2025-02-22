03b7874a495a18088a968a8fdbfc77b8cd7c80c2
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-22 09:45:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let status=document.querySelector("#status");
+let btn=document.querySelector("#enterBtn");
+
+btn.addEventListener("click",()=>{
+	status.innerHTML="<h1>Entered Metaverse<h1>";
+});
